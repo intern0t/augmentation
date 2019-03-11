097a25dfddec6562f8c31140bf4b4a203f98b12f
Prashant Shrestha <intern0t@users.noreply.github.com>
2019-03-11 12:10:53 -0400

    Initialized a new functional React project.
    Signed-off-by: Prashant Shrestha <intern0t@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./styles/build/augmentation.min.css";
+
+class App extends Component {
+  render() {
+    return <div className="App">OK!</div>;
+  }
+}
+
+export default App;
